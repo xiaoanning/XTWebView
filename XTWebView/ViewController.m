912efeffa3e9252bef41e73609c92bd6fb3d97c1
xiaoanning <734871221@qq.com>
2017-08-15 10:33:07 +0800

    web view gobak 获取当前URL

--- XTWebView/ViewController.m
+++ XTWebView/ViewController.m
@@ -8,21 +8,59 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIWebViewDelegate>
+
+
+@property ( nonatomic , strong )  UIWebView * webView ;
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self createWebView];
+
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)createWebView
+{
+    _webView = [[UIWebView alloc]initWithFrame:CGRectMake(100, 100, 300, 400)];
+    _webView.delegate = self ;
+    [self.view addSubview:_webView];
+    
+    [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"]]] ;
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+
+    return YES ;
+}
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+
+    NSLog(@" ===== %@ ",_webView.request.URL) ;
+}
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+
+}
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+
+}
+
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [_webView goBack];
+    
+    NSLog(@" %@ ",_webView.request.URL) ;
 }
 
 
